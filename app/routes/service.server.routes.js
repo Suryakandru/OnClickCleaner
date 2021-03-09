ada7a9339a24f0f861609d61c4bb69469073ed6a
Avijit <ovi.mecha@gmail.com>
2021-03-08 23:40:02 -0500

    Update and delete functionality created for booking

--- app/routes/service.server.routes.js
+++ app/routes/service.server.routes.js
@@ -11,5 +11,7 @@ module.exports = function (app) {
     app.route('/bookService').get(service.customerByUserName);
     app.post('/thanks', service.completeBookingService);
     app.route('/allBookings').get(service.bookingsByCustomer);
-    app.route('/delete').delete(service.deleteByUserName);
+    app.route('/editBooking/:_id').get(service.findServiceById).post(service.serviceByServiceId);
+    app.route('/allBookings/:_id').get(service.read).put(service.update).delete(service.deleteByServiceId);
+    app.param('_id', service.findServiceById);
 };
