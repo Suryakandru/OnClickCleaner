8b4e804d91bb1a761680a0962504949bc269130b
Tim-Gibhub <62158834+Tim-Gibhub@users.noreply.github.com>
2021-03-10 16:50:56 -0500

    Update mongoose.js
    test with dabase clund

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose');
 module.exports = function() {
 	// Use Mongoose to connect to MongoDB
 	//const db = mongoose.connect(config.db);
-	const db = mongoose.connect(config.db, {
+	
+	//user mongodb cloud
+	const db = mongoose.connect("mongodb+srv://ccc_user_16:Cccner123@cluster0.tzmwn.mongodb.net/occ_data?retryWrites=true&w=majority", {
 		useUnifiedTopology: true,
 		useNewUrlParser: true, useCreateIndex: true 
 		}).then(() => console.log('DB Connected!'))
