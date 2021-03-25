f274f99fa2f7d045c3346a82ff23d94248ff8212
Avijit <ovi.mecha@gmail.com>
2021-03-24 21:38:55 -0400

    Edit profile functionality added

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -5,6 +5,7 @@ exports.render = function (req, res) {
 		userCategory: req.user? req.user.category : '',
 		userFullName: req.user ? req.user.fullName : '',
 		username: req.user? req.user.username : '',
+		userId: req.user? req.user._id : ''
 	});
 	session.userName = req.user.username;
 	session.userFullName = req.user.fullName;
