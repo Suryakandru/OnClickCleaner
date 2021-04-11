9acb3ef9ec581c3c609e7a847bd266fd18bd5b73
Avijit <ovi.mecha@gmail.com>
2021-04-11 13:14:26 -0400

    test

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Set the 'NODE_ENV' variable
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Load the module dependencies
 const configureMongoose = require('./config/mongoose');
@@ -12,7 +12,7 @@ const configurePassport = require('./config/passport');
 
 // Create a new Mongoose connection instance
 const db = configureMongoose();
-
+const port = process.env.PORT || 3000;
 // Create a new Express application instance
 const app = configureExpress();
 
@@ -21,10 +21,10 @@ const passport = configurePassport();
 //mongoose.connect('mongodb+srv://admin:admin@cluster0.j7y8s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 //,{ useUnifiedTopology: true }, ()=> console.log("Connected to cloude DB!"));
 // Use the Express application instance to listen to the '3000' port
-app.listen(process.env.PORT);
+app.listen(port);
 
 // Log the server status to the console
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at port '+ port);
 
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app;
